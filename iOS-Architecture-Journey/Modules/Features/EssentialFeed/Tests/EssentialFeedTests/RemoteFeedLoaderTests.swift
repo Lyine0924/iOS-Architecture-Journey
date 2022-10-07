c3deaeca924e2db8f014db08580650db8cc6a974
myeongsoo <lyine0924@gmail.com>
2022-10-07 13:49:22 +0900

    RemoteFeedLoader와 HttpClient 클래스를 추가하고, 테스트 케이스를 추가했어요.

--- iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,9 +7,21 @@
 
 import XCTest
 
+class RemoteFeedLoader {
+	
+}
+
+class HTTPClient {
+	var requestedURL: URL?
+}
+
 class RemoteFeedLoaderTests: XCTestCase {
 	
-	func test() {
-
+	func test_init_doesNotRequestDataFromURL() {
+		let client = HTTPClient()
+		_ = RemoteFeedLoader()
+		
+		XCTAssertNil(client.requestedURL)
 	}
+	
 }
