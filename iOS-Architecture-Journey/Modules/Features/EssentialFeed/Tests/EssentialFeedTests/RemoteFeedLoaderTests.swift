ca8cbda1a9cde1c7e7921a3af6727380edc6206f
myeongsoo <lyine0924@gmail.com>
2022-10-07 14:30:36 +0900

    sugar 메소드를 추가하여 중복된 테스트 코드를 제거했어요.

--- iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -41,21 +41,36 @@ class HTTPClientSpy: HTTPClient {
 class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_init_doesNotRequestDataFromURL() {
-		let url = URL(string: "https://a-url.com")!
-		let client = HTTPClientSpy()
-		_ = RemoteFeedLoader(url: url, client: client)
+		let url = URL(string: "https://a-given-url.com")!
+		let (_, client) = makeSUT(url: url)
 		
 		XCTAssertNil(client.requestedURL)
 	}
 	
 	func test_load_requestDataFromURL() {
-		let url = URL(string: "https://a-url.com")!
-		let client = HTTPClientSpy()
-		let sut = RemoteFeedLoader(url: url, client: client)
+		let url = URL(string: "https://a-given-url.com")!
+		let (sut, client) = makeSUT(url: url)
 		
 		sut.load()
 		
 		XCTAssertNotNil(client.requestedURL)
 	}
 	
+}
+
+// MARK: - Helpers
+extension RemoteFeedLoaderTests {
+	private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+		let client = HTTPClientSpy()
+		let sut = RemoteFeedLoader(url: url, client: client)
+		return (sut, client)
+	}
+	
+	private class HTTPClientSpy: HTTPClient {
+		var requestedURL: URL?
+		
+		func get(from url: URL) {
+			requestedURL = url
+		}
+	}
 }
