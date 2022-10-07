4c7fcb80b3ecd1ba4331fc5fd3e39f684b30e0c0
myeongsoo <lyine0924@gmail.com>
2022-10-07 14:23:15 +0900

    RemoteFeedLoader | 하드코딩된 url을 프로퍼티로 변경했어요.

--- iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,13 +10,18 @@ import XCTest
 class RemoteFeedLoader {
 	
 	let client: HTTPClient
+	let url: URL
 	
-	init(client: HTTPClient) {
+	init(
+		url: URL,
+		client: HTTPClient
+	) {
+		self.url = url
 		self.client = client
 	}
 	
 	func load() {
-		client.get(from: URL(string: "https://a-url.com")!)
+		client.get(from: url)
 	}
 }
 
@@ -36,15 +41,17 @@ class HTTPClientSpy: HTTPClient {
 class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_init_doesNotRequestDataFromURL() {
+		let url = URL(string: "https://a-url.com")!
 		let client = HTTPClientSpy()
-		_ = RemoteFeedLoader(client: client)
+		_ = RemoteFeedLoader(url: url, client: client)
 		
 		XCTAssertNil(client.requestedURL)
 	}
 	
 	func test_load_requestDataFromURL() {
+		let url = URL(string: "https://a-url.com")!
 		let client = HTTPClientSpy()
-		let sut = RemoteFeedLoader(client: client)
+		let sut = RemoteFeedLoader(url: url, client: client)
 		
 		sut.load()
 		
