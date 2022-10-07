6b2dc1d6274ecf3539daf7d82cf1303e780ffb9c
myeongsoo <lyine0924@gmail.com>
2022-10-07 14:21:23 +0900

    HTTPClient를 프로토콜 타입으로 변경했어요.

--- iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,20 +9,24 @@ import XCTest
 
 class RemoteFeedLoader {
 	
+	let client: HTTPClient
+	
+	init(client: HTTPClient) {
+		self.client = client
+	}
+	
 	func load() {
-		HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+		client.get(from: URL(string: "https://a-url.com")!)
 	}
 }
 
-class HTTPClient {
-	static var shared = HTTPClient()
-	
-	func get(from url: URL) {}
+protocol HTTPClient {
+	func get(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
 	
-	override func get(from url: URL) {
+	func get(from url: URL) {
 		requestedURL = url
 	}
 	
@@ -33,15 +37,14 @@ class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_init_doesNotRequestDataFromURL() {
 		let client = HTTPClientSpy()
-		HTTPClient.shared = client
-	
+		_ = RemoteFeedLoader(client: client)
+		
 		XCTAssertNil(client.requestedURL)
 	}
 	
 	func test_load_requestDataFromURL() {
 		let client = HTTPClientSpy()
-		HTTPClient.shared = client
-		let sut = RemoteFeedLoader()
+		let sut = RemoteFeedLoader(client: client)
 		
 		sut.load()
 		
