35fb8e7da6f797472bdb08e314666b8e845124fa
myeongsoo <lyine0924@gmail.com>
2022-10-07 12:52:34 +0900

    FeedItem을 추가했어요.

--- iOS-Architecture-Journey/Modules/Features/EssentialFeed/Sources/EssentialFeed/EssentialFeed.swift
+++
@@ -1,6 +0,0 @@
-public struct EssentialFeed {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ iOS-Architecture-Journey/Modules/Features/EssentialFeed/Sources/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//
+//
+//  Created by Lyine on 2022/10/07.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
