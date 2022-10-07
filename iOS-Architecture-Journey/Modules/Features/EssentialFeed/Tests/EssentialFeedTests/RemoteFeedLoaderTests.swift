788b141560da93b6966e358005f0c35580429b36
myeongsoo <lyine0924@gmail.com>
2022-10-07 14:19:15 +0900

    HTTPClientSpy를 추가하고, 테스트 로직을 HttpClient로부터 분리했어요.

--- iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ iOS-Architecture-Journey/Modules/Features/EssentialFeed/Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,14 +10,21 @@ import XCTest
 class RemoteFeedLoader {
 	
 	func load() {
-		HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+		HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
 	}
 }
 
 class HTTPClient {
-	static let shared = HTTPClient()
+	static var shared = HTTPClient()
 	
-	private init() {}
+	func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
+	
+	override func get(from url: URL) {
+		requestedURL = url
+	}
 	
 	var requestedURL: URL?
 }
@@ -25,14 +32,15 @@ class HTTPClient {
 class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_init_doesNotRequestDataFromURL() {
-		let client = HTTPClient.shared
-		_ = RemoteFeedLoader()
-		
+		let client = HTTPClientSpy()
+		HTTPClient.shared = client
+	
 		XCTAssertNil(client.requestedURL)
 	}
 	
 	func test_load_requestDataFromURL() {
-		let client = HTTPClient.shared
+		let client = HTTPClientSpy()
+		HTTPClient.shared = client
 		let sut = RemoteFeedLoader()
 		
 		sut.load()
